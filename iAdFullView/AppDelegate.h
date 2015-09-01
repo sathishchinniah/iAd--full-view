40cdc681ce4d49bfb3bb7ffd5faaf162b0e9a121
sathish chinniah <sathish73ec@gmail.com>
2015-09-02 03:06:32 +0530

    Initial Commit

---
+++ iAdFullView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAdFullView
+//
+//  Created by Sathish Chinniah on 02/09/15.
+//  Copyright (c) 2015 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iAdFullView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iAdFullView
+//
+//  Created by Sathish Chinniah on 02/09/15.
+//  Copyright (c) 2015 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
